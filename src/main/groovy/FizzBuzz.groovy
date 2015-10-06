81c4d04f83fc08a27c2cf58f64bd8783e83bf747
Don Abney <tdonaldabney@gmail.com>
2015-10-06 08:21:34 -0400

    Beginning a template for kata battle

---
+++ src/main/groovy/FizzBuzz.groovy
@@ -0,0 +1,6 @@
+class FizzBuzz {
+    
+    public static process(int input) {
+        ((input % 3 == 0 ? 'Fizz' : '') + (input % 5 == 0 ? 'Buzz' : '')) ?: input
+    }
+}
---
+++ src/test/groovy/FizzBuzzAcceptTest.groovy
@@ -0,0 +1,11 @@
+import org.junit.Test
+
+class FizzBuzzAcceptTest {
+
+    @Test
+    void shouldDisplayCorrectlyFrom1To100() {
+        (1..100).each { testNumber ->
+            println FizzBuzz.process(testNumber)
+        }
+    }
+}
