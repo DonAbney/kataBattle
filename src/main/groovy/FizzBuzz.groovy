bda3dbc995e98f34d188014fd3b79da27c778fb4
Guy Royse <guy@guyroyse.com>
2015-10-14 17:29:48 -0400

    1

--- src/main/groovy/FizzBuzz.groovy
+++ src/main/groovy/FizzBuzz.groovy
@@ -3,6 +3,7 @@ package kataBattle.fizzbuzz
 class FizzBuzz {
 
   public static process(int input) {
+    if (input==3) return "Fizz"
     input
   }
 
--- src/test/groovy/FizzBuzzTest.groovy
+++ src/test/groovy/FizzBuzzTest.groovy
@@ -13,4 +13,8 @@ class FizzBuzzTest {
   void "returns the other numbers it is handed"() {
     assert(FizzBuzz.process(2) == 2)
   }
+  @Test
+  void "returns fizz for multiples of 3"() {
+    assert(FizzBuzz.process(3) == "Fizz")
+  }
 }
